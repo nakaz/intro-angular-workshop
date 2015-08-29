b2336b2e69535b0271658555b50b9442d2526a12
Sean Nakamura <seanenakamura@gmail.com>
2015-08-29 13:28:15 -1000

    added server listening

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+
+var server = app.listen(8080, function (){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('buggah workin at http://%s%s', host, port);
+});
