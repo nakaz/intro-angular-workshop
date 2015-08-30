3bcd6b521a89ffb3a6de5a9d0fc21f223a233e66
Sean Nakamura <seanenakamura@gmail.com>
2015-08-29 18:48:45 -1000

    basic filtering in html

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ app.use(express.static('public'));
 
 app.get('/api', function (req, res, next){
   res.json([
-      { title: 'Field of Dreams'},
-      { title: 'Moneyball'},
-      { title: 'The Rookie'},
-      { title: 'Angels in the Outfield'},
-      { title: 'A League of their Own'},
-      { title: '42'},
-      { title: 'Major League'}
+      { title: 'Field of Dreams', year: 1989},
+      { title: 'Moneyball', year: 2004},
+      { title: 'The Rookie', year: 2002},
+      { title: 'Angels in the Outfield', year: 1994},
+      { title: 'A League of their Own', year: 1992},
+      { title: '42', year: 2013},
+      { title: 'Major League', year: 1989},
     ]);
 });
 
