289efc79d420936d5ef8f6d8e6f410237539ad4e
Sean Nakamura <seanenakamura@gmail.com>
2015-08-29 20:01:35 -1000

    basic routing for index

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,26 @@
 (function (){
-  var app = angular.module('myApp', ['ngResource']);
+  var app = angular.module('myApp', ['ngResource', 'ngRoute']);
+
+  //routes
+  app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      // .when('/books', {
+      //   templateUrl: 'views/books.html',
+      //   controller: 'BooksController'
+      // })
+      // .when('/movies', {
+      //   templateUrl: 'views/movies.html',
+      //   controller: 'MoviesController'
+      // })
+      // .when('/other', {
+      //   templateUrl: 'views/other.html',
+      //   controller: 'OtherController'
+      // })
+      .when('/', {
+        templateUrl: 'views/default.html',
+        controller: 'myController'
+      });
+  }]);
 
   //runs after app is loaded
   app.run(function ($rootScope, APP_VERSION){
