ad8dd038b12e3f25500f101bdaeb185dde698cbc
Sean Nakamura <seanenakamura@gmail.com>
2015-08-29 19:38:08 -1000

    add custom filter including input field

--- public/js/filters.js
+++ public/js/filters.js
@@ -2,6 +2,10 @@
   angular
     .module('myApp')
     .filter('beforeYearFilter', function (){
-
+      return function (movies, year){
+        return movies.filter(function (movie){
+          return movie.year < year;
+        });
+      };
     });
 })();
